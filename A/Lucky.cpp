d8b8e1e1dfd6b4e8b2f2db377fa464ea1db7aaa9
Ahmed Fathy <fatahmed1928@gmail.com>
2024-10-17 20:08:27 +0300

    Guess what

---
+++ A/Lucky.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string s;
+
+        cin >> s;
+
+        if (int(s[0]) + int(s[1]) + int(s[2]) == int(s[3]) + int(s[4]) + int(s[5]))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
