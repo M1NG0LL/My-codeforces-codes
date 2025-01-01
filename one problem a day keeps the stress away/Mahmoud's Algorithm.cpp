f20f3f1aa2ebf034ec77e40b6ff4098f57114709
Ahmed Fathy <fatahmed1928@gmail.com>
2025-01-01 19:42:41 +0200

    ...

---
+++ one problem a day keeps the stress away/Mahmoud's Algorithm.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    ll n;
+    cin >> n;
+
+    while (n != 1)
+    {
+        cout << n << " ";
+        if (n % 2 == 0)
+            n /= 2;
+        else
+            n = (n * 3) + 1;
+    }
+
+    cout << 1 << " ";
+}
